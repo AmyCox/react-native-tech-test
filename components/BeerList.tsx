6bc90c513335f658850a9aa3687b2adc9b647789
Amy Cox <amy_cox@me.com>
2024-02-03 14:44:18 +0000

    wrapped request in a useEffect

--- components/BeerList.tsx
+++ components/BeerList.tsx
@@ -1,29 +1,31 @@
-import { Text, View } from 'react-native';
-import axios, { AxiosResponse } from 'axios';
-
-
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import axios, { AxiosResponse } from "axios";
 
 const BeerList: React.FC = () => {
+  const [beers, setBeers] = useState([]);
+  console.log("here are the beers", beers);
 
+  useEffect(() => {
     const fetchData = async () => {
-        try {
-            const response: AxiosResponse = await axios.get('https://api.punkapi.com/v2/beers?page=1&per_page=10');
-            console.log(response.data);
-
-        }   
-        catch (error) {
-            console.error(error);
-        }   
-    }
+      try {
+        const response: AxiosResponse = await axios.get(
+          "https://api.punkapi.com/v2/beers?page=1&per_page=10"
+        );
+        console.log(response.data);
+        setBeers(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
     fetchData();
-    
+  }, []);
 
-
-    return (
-        <View>
-            <Text>Beer List</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Beer List</Text>
+    </View>
+  );
+};
 
 export default BeerList;
